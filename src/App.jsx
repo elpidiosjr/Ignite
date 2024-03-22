73869104ef7ce4c1b9c1e2b286d9b0022b24df87
unknown <elpidiosoares@gmail.com>
2024-03-22 18:36:29 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import {Post} from "./Post"
+import { Post } from "./Post"
 
-function App () {
+export function App() {
   return (
     <div>
-   <Button />
-   <Button />
-   <Button />
-   <Button />
-   <Button />
+  <Post
+    author="Elpídio Soares"
+    content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet atque corrupti eius aspernatur ullam cum consectetur quas repudiandae soluta magni similique tenetur quae delectus ad ratione, iusto nihil? Libero, reiciendis?"
+  />
+  <Post
+    author="Leorio Zanbrusk"
+    content="Nada melhor que um dia após o outro."  
+  />  
    </div>
   )
 }
-
 
